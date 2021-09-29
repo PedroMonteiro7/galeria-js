0f761665c22ebfdb30ea96b8f87f443c99fbd622
PedroMonteiro7 <21192977@CFP127ALUNO.LOCAL>
2021-09-29 10:36:30 -0300

    Carregando as imagens com JS

--- galeria.js
+++ galeria.js
@@ -14,17 +14,39 @@ const imagens = [
 //map retorna um novo array. foreach apenas percorre
 //innerHTML = innerHTML +   ==   innerHTML +=
 
+//replace substitui "./img/" por "" e split separa por ponto e mantém apenas a 1ª parte
+const limparId = (url) => url.replace("./img/", "").split(".")[0]
+
 const criarItem = (urlImagem) => {
     const container = document.querySelector(".galeria-container")
 
     const novoLink = document.createElement("a")
-    novoLink.href = urlImagem
+    novoLink.href = `#${limparId(urlImagem)}`
     novoLink.classList.add("galeria-itens")
     novoLink.innerHTML = `<img src="${urlImagem}" alt="">`
 
     container.appendChild(novoLink)
 }
 
-const carregarImagens = () => imagens.forEach(criarItem)
+const carregarGaleria = () => imagens.forEach(criarItem)
 
-carregarImagens()
+const criarSlide = (urlImagem) => {
+    const container = document.querySelector(".slide-container")
+    const novoDiv = document.createElement("div")
+    novoDiv.classList.add("slide")
+    novoDiv.id = limparId(urlImagem)
+    novoDiv.innerHTML = `
+        <div class="imagem-container">
+            <a href="" class="icones fechar">&#128473;</a>
+            <a href="#zenitsu-agatsuma" class="icones anterior">&#171;</a>
+            <img src="${urlImagem}" alt="">
+            <a href="#giyu-tomioka" class="icones proximo">&#187;</a>
+        </div>
+    `
+    container.appendChild(novoDiv)
+}
+
+const carregarSlide = (imagens) => imagens.forEach(criarSlide)
+
+carregarGaleria(imagens)
+carregarSlide(imagens)
